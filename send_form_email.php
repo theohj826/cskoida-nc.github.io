ec27ef6d5aefd11c9a3e294f2f5da98aa83d90ca
theohj826 <theohj826@gmail.com>
2017-07-14 11:37:08 -0400

    finance page added

--- send_form_email.php
+++ send_form_email.php
@@ -81,7 +81,7 @@ $headers = 'From: '.$email_from."\r\n".
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
-<!-- include your own success html here -->
+
  
 Thank you for contacting us. We will be in touch with you very soon.
  
